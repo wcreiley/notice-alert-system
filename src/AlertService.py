fae346e0634c401d00d94faa933c2117ae4cb1de
Wesley Reiley <wesley_reiley@yahoo.com>
2025-05-06 11:06:50 -0500

    Issue #28: Initial alert service

---
+++ src/AlertService.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+class AlertService:
+
+    def __init__(self):
+        print("AlertService initialized")
+        load_dotenv()
+        self.api_host = os.environ.get("PATHWAY_REST_CONNECTOR_HOST", "127.0.0.1")
+        self.api_port = int(os.environ.get("PATHWAY_REST_CONNECTOR_PORT", 8080))
+        self.url = f"http://{self.api_host}:{self.api_port}/"
+
+    def build_capacity_constraint_prompt(self):
+        return """
+            Alert me of any capacity constraints.
+            
+            Identify the notice type.
+            Identify the location.
+            Identify the pipeline segment.
+            Identify the duration of the outage.
+            Identify the change in capacity.
+        """
+
+    def build_site_capacity_constraint_prompt(self):
+        return """
+            Alert me of the site names of any capacity constraints.
+        """
+
+    def run(self):
+        print("Setting up static alert prompts")
+
+        active_prompts = [
+            self.build_site_capacity_constraint_prompt(),
+            self.build_capacity_constraint_prompt()
+        ]
+
+        for prompt in active_prompts:
+            data = {"query": prompt, "user": "user"}
+
+            print(f"Prompt: {data}")
+            response = requests.post(self.url, json=data)
+            print(f"Response: {str(response.content)}")
+
+
+if __name__ == "__main__":
+    AlertService().run()
+
